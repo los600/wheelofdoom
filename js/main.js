15e73b20518dea0e256976846873bce9b8552c14
Marta <martaguitart938@gmail.com>
2021-10-28 10:52:56 +0200

    delete image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+let codersList =[];
+
+function addCoderList(name){
+codersList.push(name);
+
+}
+addCoderList("marta");
+console.log(codersList)
+
+function getValue(){
+    let inputId = document.getElementById("cartas");
+    addCoderList(inputId.value);
+    console.log(codersList)
+}
