5a3b6b1ccab981db04ce57ca32f4be64953bad18
solidmaradini <solidmaradini@gmail.com>
2021-11-03 09:17:15 +0100

    staff JS for functions and class

--- js/pagina2.js
+++ js/pagina2.js
@@ -1,6 +1,6 @@
 // Arrays:
 let userName = [];
-let userImage = ["img.1", "img.2", "img.3"];
+// let userImage = ["img.1", "img.2", "img.3"];
 
 // funcion para enviar "nombres/datos" pusheandolos al array
 function addplayer(newPlayer) {
